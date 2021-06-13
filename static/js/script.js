2e8dbc2257983bd40faec1e02d29e70a8a755262
Andre Fernandes Cristofolini Melo <andre.melo@dotgroup.com.br>
2021-06-12 21:56:41 -0300

    all

--- static/js/script.js
+++ static/js/script.js
@@ -1,6 +1,7 @@
 (function($){
     $('.game').on('click', function(){
         $('.game-option').text($(this).text());
+        $('.link').attr("href",`https://firebase-middleware-2.herokuapp.com/${$(this).text()}`)
     })
 
 })(jQuery);
